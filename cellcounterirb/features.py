25e1b38e8926ce42fd32935458d56837bf3b6bb6
CristianAlan-ui <floresflorescristianalan@gmail.com>
2026-05-03 11:18:34 -0600

    features.py

--- cellcounterirb/features.py
+++ cellcounterirb/features.py
@@ -1,14 +1,36 @@
 import numpy as np
 import cv2
-def HarrisCorner(img):
+def HarrisCorner(img,blockSize, ksize, k):
+     """
+    Detects the Harris corner response of an input image.
+
+    Parameters:
+        img (ndarray): Input image (must be in gray scale).
+        blockSize (int): Distance or neighborhood used for the corner detection.
+        ksize (int): Parameter of sobel operator.
+        k (float): Sensitivity.
+    Returns:
+        harris (ndarray): Harris response map indicating corner strength.
+    """
     img_float = np.float32(img)
-    harris = cv2.cornerHarris(img_float, blockSize=2, ksize=3, k=0.04)
+    harris = cv2.cornerHarris(img_float, blockSize, ksize, k)
     harris = cv2.dilate(harris, None)
     return harris
 def HarrisVis(img):
-    img_float = np.float32(img)
-    harris = cv2.cornerHarris(img_float, blockSize=2, ksize=3, k=0.04)
+    """
+    Detects and visualizes Harris corners on the input image.
+    Parameters:
+        img (ndarray): Input image (must be grayscale).
+        blockSize (int): Distance or neighborhood used for the corner detection.
+        ksize (int): Parameter of sobel operator.
+        k (float): Sensitivity.
+    Returns:
+        harris_vis (ndarray): Original image with detected corners highlighted in red.
+    """
+    img_float = np.float32(img, blockSize, ksize, k)
+    harris = cv2.cornerHarris(img_float, blockSize, ksize, k)
     harris = cv2.dilate(harris, None)
     harris_vis = img.copy()
     harris_vis[harris > 0.01 * harris.max()] = [0, 0, 255]
     return harris_vis
+    
