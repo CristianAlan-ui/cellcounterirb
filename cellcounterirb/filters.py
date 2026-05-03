bd737389c75f5bc36433193d1a4d7342bed45f98
CristianAlan-ui <floresflorescristianalan@gmail.com>
2026-05-03 11:02:37 -0600

    filters.py

--- cellcounterirb/filters.py
+++ cellcounterirb/filters.py
@@ -1,6 +1,17 @@
 import numpy as np
 import cv2
 def LowPass(img, freq):
+    def HoughCircle(img):
+    """
+    Applies a low-pass filter in the frequency domain and converts the result
+    back to the spatial domain.
+    
+    Parameters:
+        img (ndarray): Input image for the low-pass filter (must be grayscale).
+        freq(int): cuttoff frequency of the filter
+    Returns:
+        img_filt (ndarray): Filtered image in the spatial domain.
+    """
     rows, cols = img.shape
     crow, ccol = rows//2,cols//2
     mask = np.zeros((rows,cols))
@@ -15,6 +26,16 @@ def LowPass(img, freq):
     img_filt = np.uint8(img_filt)
     return img_filt
 def HighPass(img, freq):
+    """
+    Applies a high-pass filter in the frequency domain and converts the result
+    back to the spatial domain.
+    
+    Parameters:
+        img (ndarray): Input image for the high-pass filter (must be grayscale).
+        freq(int): cuttoff frequency of the filter
+    Returns:
+        img_filt (ndarray): Filtered image in the spatial domain.
+    """
     rows, cols = img.shape
     crow, ccol = rows//2,cols//2
     mask = np.ones((rows, cols))
@@ -29,6 +50,17 @@ def HighPass(img, freq):
     img_filt = np.uint8(img_filt)
     return img_filt
 def BandPass(img, freq, dist):
+    """
+    Applies a band-pass filter in the frequency domain and converts the result
+    back to the spatial domain.
+    
+    Parameters:
+        img (ndarray): Input image for the band-pass filter (must be grayscale).
+        freq(int): cuttoff frequency of the filter
+        dist(int): Sets the band frequency in the filter (starting from the freq parameter)
+    Returns:
+        img_filt (ndarray): Filtered image in the spatial domain.
+    """
     rows, cols = img.shape
     crow, ccol = rows//2,cols//2
     mask = np.zeros((rows, cols))
@@ -43,6 +75,17 @@ def BandPass(img, freq, dist):
     img_filt = np.uint8(img_filt)
     return img_filt
 def BandStop(img, freq, dist):
+    """
+    Applies a band-stop filter in the frequency domain and converts the result
+    back to the spatial domain.
+    
+    Parameters:
+        img (ndarray): Input image for the band-stop filter (must be grayscale).
+        freq(int): cuttoff frequency of the filter
+        dist(int): Sets the band frequency in the filter (starting from the freq parameter)
+    Returns:
+        img_filt (ndarray): Filtered image in the spatial domain.
+    """
     rows, cols = img.shape
     crow, ccol = rows//2,cols//2
     mask = np.ones((rows, cols))
