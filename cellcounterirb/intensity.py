9843bce0613a1bb6d6f80f2df54f08797b18a651
CristianAlan-ui <floresflorescristianalan@gmail.com>
2026-05-03 11:48:11 -0600

    intensity.py

--- cellcounterirb/intensity.py
+++ cellcounterirb/intensity.py
@@ -1,6 +1,16 @@
 import numpy as np
 
 def Logarithmic(img, c):
+    """
+    Applies logarithmic intensity transformation to enhance dark regions of an image.
+
+    Parameters:
+        img (ndarray): Input image.
+        c (float): Transformation strength.
+
+    Returns:
+        log_img (ndarray): Resulting image of the logarithmic transformation
+    """
     img = img.astype(np.float32)
     log_img = np.log(c+img) 
     log_img = (log_img - log_img.min())/(log_img.max()-log_img.min())
@@ -8,6 +18,16 @@ def Logarithmic(img, c):
     return log_img
 
 def Contrast(img, m, E):
+    """
+    Applies contrast using a nonlinear transformation
+    Parameters:
+        img (ndarray): Input image.
+        m (float): Midpoint control.
+        E (float): Exponent.
+
+    Returns:
+        s (ndarray): Returns the transformed image
+    """
     img = img.astype(np.float32)
     s = 1 / (1+(m/img))**E
     s = (s - s.min())/(s.max()-s.min())
