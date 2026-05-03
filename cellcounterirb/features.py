95e7c73f154c120a224f85747555e9729c8b3d18
Cristian Alan Flores Flores <floresflorescristianalan@gmail.com>
2026-05-03 13:31:14 -0600

    features.py

--- cellcounterirb/features.py
+++ cellcounterirb/features.py
@@ -1,14 +1,16 @@
 import numpy as np
 import cv2
+
 def HarrisCorner(img, blockSize, ksize, k):
-     """
+    """
     Detects the Harris corner response of an input image.
 
     Parameters:
-        img (ndarray): Input image (must be in gray scale).
-        blockSize (int): Distance or neighborhood used for the corner detection.
-        ksize (int): Parameter of sobel operator.
-        k (float): Sensitivity.
+        img (ndarray): Input image (must be grayscale).
+        blockSize (int): Neighborhood size for corner detection.
+        ksize (int): Aperture parameter of Sobel operator.
+        k (float): Harris detector free parameter.
+
     Returns:
         harris (ndarray): Harris response map indicating corner strength.
     """
@@ -16,21 +18,27 @@ def HarrisCorner(img, blockSize, ksize, k):
     harris = cv2.cornerHarris(img_float, blockSize, ksize, k)
     harris = cv2.dilate(harris, None)
     return harris
+
+
 def HarrisVis(img, blockSize, ksize, k):
     """
     Detects and visualizes Harris corners on the input image.
+
     Parameters:
         img (ndarray): Input image (must be grayscale).
-        blockSize (int): Distance or neighborhood used for the corner detection.
-        ksize (int): Parameter of sobel operator.
-        k (float): Sensitivity.
+        blockSize (int): Neighborhood size for corner detection.
+        ksize (int): Aperture parameter of Sobel operator.
+        k (float): Harris detector free parameter.
+
     Returns:
-        harris_vis (ndarray): Original image with detected corners highlighted in red.
+        harris_vis (ndarray): Image with detected corners highlighted in red.
     """
-    img_float = np.float32(img, blockSize, ksize, k)
+    img_float = np.float32(img)
+
     harris = cv2.cornerHarris(img_float, blockSize, ksize, k)
     harris = cv2.dilate(harris, None)
+
     harris_vis = img.copy()
     harris_vis[harris > 0.01 * harris.max()] = [0, 0, 255]
+
     return harris_vis
-    
