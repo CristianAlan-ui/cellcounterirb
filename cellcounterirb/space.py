2230e740e28363e8faa9bc3a2e2a9acb466d5b52
CristianAlan-ui <floresflorescristianalan@gmail.com>
2026-05-03 12:04:05 -0600

    space.py

--- cellcounterirb/space.py
+++ cellcounterirb/space.py
@@ -2,12 +2,32 @@ import numpy as np
 import cv2
 from scipy import signal
 def Average(img, tam):
+    """
+    Applies an average filter to smooth an image.
+
+    Parameters:
+        img (ndarray): Input image.
+        tam (int): Size of the square kernel (tam x tam).
+
+    Returns:
+        av (ndarray): Smoothed image.
+    """
     kp = np.ones((tam,tam))
     kp = kp/sum(kp.ravel())
-    prom = cv2.filter2D(img,-1,kp)
-    return prom
+    av = cv2.filter2D(img,-1,kp)
+    return av
 def Gaussian(img, size, sigma):
+    """
+    Applies a Gaussian blur filter to smooth an image.
 
+    Parameters:
+        img (ndarray): Input image.
+        size (int): Size of the Gaussian kernel (must be odd).
+        sigma (float): Standard deviation of the Gaussian distribution.
+
+    Returns:
+        img (ndarray): Gaussian image.
+    """
     m = size//2
     x,y = np.meshgrid(np.arange(-m,m+1), np.arange(-m, m+1))
     k = (1/(2*3.1416*sigma**2))*np.exp(-(x**2+y**2)/(2*sigma**2))
@@ -15,11 +35,20 @@ def Gaussian(img, size, sigma):
     img = cv2.filter2D(img, -1,k)
     return img
 def Convolve(img, h):
-    #donde h es un kernel invertible
+    """
+    Applies a 2D convolution between an image and a kernel.
+
+    Parameters:
+        img (ndarray): Input image.
+        h (ndarray): Convolution kernel.
+
+    Returns:
+        conv (ndarray): Result of convolution operation.
+    """
     h = h[::-1, ::-1]
     conv = signal.convolve2d(img, h, mode = 'same')
     return conv
-#Deteccion de bordes: gradiente, sobel, prewitt y laplace
+#Border detection
 
 def Gradient(img, Hx):
     dx = signal.convolve2d(img, Hx, mode='same')
